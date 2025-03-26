266589748705a1ac4d76e3268a70c2dbca616139
Dabizu <dabizu100@gmail.com>
2025-03-26 12:40:55 -0600

    actualizacion de tabla

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 import React, { useState, useEffect } from 'react';
-
+//import React, { useState } from 'react';
 function App() {
-  let array=[];
-  let subArray=[];
+  /*let array=[];
+  let subArray=[];*/
   const [datos, setDatos] = useState([]);
   const [loading, setLoading] = useState(true); // Para mostrar un indicador de carga
   const listar=()=>{
     fetch('http://sistemasupport.site/scsystemsv4/php/conection/acciones/listarHistorial.php')
         .then(response => response.json())
         .then(data =>{
+          console.log(data)
           //var plantilla=``;
-          array=data;
+          //array=data;
           setDatos(data);
           setLoading(false); // Termina la carga
+          console.log("estas es la variable con datos",datos);
           /*
           data.forEach(element => {
             plantilla=plantilla+`
@@ -43,47 +45,49 @@ function App() {
           //document.getElementById("contenedor").innerHTML=plantilla;
         });
   }
-  listar();
+  useEffect(()=>{
+    listar();
+  },[])
 
   return (
     <div className="App">
-      
       <div id='main'></div>
       <div>
         {loading ? (
           <p>Cargando datos...</p>
         ) : (
-            <table class="table">
-                <thead>
-                <tr>
-                    <th scope="col">Folio</th>
-                    <th scope="col">fecha</th>
-                    <th scope="col">cliente</th>
-                    <th scope="col">telefono</th>
-                    <th scope="col">precio</th>
-                    <th scope="col">status</th>
-                    <th scope="col">equipo</th>
-                    <th scope="col">cargador</th>
+          <table className="table">
+            <thead>
+              <tr>
+                <th scope="col">Folio</th>
+                <th scope="col">fecha</th>
+                <th scope="col">cliente</th>
+                <th scope="col">telefono</th>
+                <th scope="col">precio</th>
+                <th scope="col">status</th>
+                <th scope="col">equipo</th>
+                <th scope="col">cargador</th>
+              </tr>
+            </thead>
+            <tbody id='contenedor'>
+              {datos.map((element, index) => (
+                <tr key={index}>
+                  <td>{element.folio}</td>
+                  <td>{element.fecha}</td>
+                  <td>{element.cliente}</td>
+                  <td>{element.telefono}</td>
+                  <td>{element.precio}</td>
+                  <td>{element.status}</td>
+                  <td>{element.equipo}</td>
+                  <td></td>
                 </tr>
-                </thead>
-                <tbody id='contenedor'>
-                  {datos.map((element)=>{
-                    <tr key={element.id}>
-                      <td>${element.folio}</td>
-                      <td>${element.fecha}</td>
-                      <td>${element.cliente}</td>
-                      <td>${element.telefono}</td>
-                      <td>${element.precio}</td>
-                      <td>${element.status}</td>
-                      <td>${element.equipo}</td>
-                      <td></td>
-                    </tr>
-                  })}
-                </tbody>
-            </table>
+              ))}
+            </tbody>
+          </table>
         )}
-        </div>
+      </div>
     </div>
+
   );
 }
 
